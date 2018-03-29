4d33bd76aa4e0e59f08cc06d87a628f8ca384026
Yevhen Shemet <yevhene@gmail.com>
2018-03-29 23:41:33 +0300

    Android integration

--- demo/ReactNativeNavigationNavigation/android/app/src/main/java/com/reactnativenavigationnavigation/MainApplication.java
+++ demo/ReactNativeNavigationNavigation/android/app/src/main/java/com/reactnativenavigationnavigation/MainApplication.java
@@ -1,47 +1,32 @@
 package com.reactnativenavigationnavigation;
 
-import android.app.Application;
+import com.reactnativenavigation.NavigationApplication;
 
-import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
+public class MainApplication extends NavigationApplication {
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
+
+  @Override
+  public String getJSMainModuleName() {
+    return "index";
+  }
+
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+      new VectorIconsPackage()
+    );
   }
 }
